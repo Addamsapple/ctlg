5b0b3fcff88a0d086a033cb2cabd322bc8cd4342
Justin T <jtullek@gmail.com>
2023-04-05 14:47:27 +0200

    Implement outstanding EditField functions

--- src/com/edit_field.cpp
+++ src/com/edit_field.cpp
@@ -4,29 +4,27 @@
 #include "io.h"
 #include "interface.h"
 #include "render.h"
-
-//need access to catalogue's _itemConstructor to use the correct reassign method
-//or just define a reassignItemField method, etc to access it use it 
-
+#include "return.h"
 
 //rename class to EditItemField???
 bool EditField::_execute(StringVector arguments) {
 	_item = startingItem + selectedItem;
 	_position = startingItemColumn + selectedItemColumn;
-	//check if item/field actually exists
-	_string = catalogue[_item][_position]->string();
-	setOutput(_string);
-	if (getInput()) {
-		catalogue.reassignField(std::string(ioString.data()), _item, _position);
-		return true;
+	setOutput(catalogue[_item][_position]->string());
+	while (getInput()) {
+		_field = std::unique_ptr<Field>(catalogue.itemConstructor()[_position]->construct(ioString.data()));
+		if (returnCode() == 0) {
+			catalogue[_item][_position].swap(_field);
+			return true;
+		}
 	}
 	return false;
 }
 
 void EditField::undo() {
-
+	catalogue[_item][_position].swap(_field);
 }
 
 void EditField::redo() {
-
+	catalogue[_item][_position].swap(_field);
 }
--- src/com/edit_field.h
+++ src/com/edit_field.h
@@ -1,12 +1,14 @@
+#include <memory>
+
 #include "command.h"
+#include "field.h"
 
 class EditField : public UndoableCommand {
 	private:
 		//think of better member names???
 		size_t _item;
 		size_t _position;
-		//std::unique_ptr<Field> field;
-		std::string _string;//temporary, use until copy functionality developed
+		std::unique_ptr<Field> _field;
 
 	OVERRIDE_UNDOABLE_MEMBER_FUNCTIONS
 };
