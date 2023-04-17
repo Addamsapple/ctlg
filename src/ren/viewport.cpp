24743e4214e2905778ce323dc39eb29282a9b8ff
Justin T <jtullek@gmail.com>
2023-04-17 16:54:35 +0200

    Rename private Viewport functions/arguments

--- src/ren/viewport.cpp
+++ src/ren/viewport.cpp
@@ -21,38 +21,37 @@ int Viewport::totalElements() const {
 }
 
 template<const int & (*Function)(const int &, const int &)>
-int moveTowardBound(int &element, int elements, int elementBound) {
-	int moved = Function(element + elements, elementBound) - element;
-	element += moved;
-	return moved;
+int clamp(int &value, int increment, int limit) {
+	int result = Function(value + increment, limit) - value;
+	value += result;
+	return result;
 }
 
 void Viewport::refresh() {
-	if (int scrolled = moveTowardBound<std::min<int>>(_firstElement, 0, std::max(_totalElements() - viewableElements(), 0)))
-		moveTowardBound<std::min<int>>(_selectedElement, -scrolled, std::min(_viewableElements(), _totalElements()) - 1);
-	else if (int moved = moveTowardBound<std::min<int>>(_selectedElement, 0, std::min(_viewableElements(), _totalElements()) - 1))
-		moveTowardBound<std::min<int>>(_firstElement, -moved, std::max(_totalElements() - viewableElements(), 0));
+	if (int scrolled = clamp<std::min<int>>(_firstElement, 0, std::max(_totalElements() - viewableElements(), 0)))
+		clamp<std::min<int>>(_selectedElement, -scrolled, std::min(_viewableElements(), _totalElements()) - 1);
+	else if (int moved = clamp<std::min<int>>(_selectedElement, 0, std::min(_viewableElements(), _totalElements()) - 1))
+		clamp<std::min<int>>(_firstElement, -moved, std::max(_totalElements() - viewableElements(), 0));
 }
 
 void Viewport::moveBackward(int elements) {
-	moveTowardBound<std::max<int>>(_firstElement, -elements - moveTowardBound<std::max<int>>(_selectedElement, -elements, 0), 0);
+	clamp<std::max<int>>(_firstElement, -elements - clamp<std::max<int>>(_selectedElement, -elements, 0), 0);
 }
 
 void Viewport::moveForward(int elements) {
-	moveTowardBound<std::min<int>>(_firstElement, elements - moveTowardBound<std::min<int>>(_selectedElement, elements, std::min(_viewableElements(), _totalElements()) - 1), std::max(_totalElements() - _viewableElements(), 0));
+	clamp<std::min<int>>(_firstElement, elements - clamp<std::min<int>>(_selectedElement, elements, std::min(_viewableElements(), _totalElements()) - 1), std::max(_totalElements() - _viewableElements(), 0));
 }
 
 void Viewport::scrollBackward(int elements) {
-	moveTowardBound<std::min<int>>(_selectedElement, -moveTowardBound<std::max<int>>(_firstElement, -elements, 0), std::min(_viewableElements(), _totalElements()) - 1);
+	clamp<std::min<int>>(_selectedElement, -clamp<std::max<int>>(_firstElement, -elements, 0), std::min(_viewableElements(), _totalElements()) - 1);
 }
 
 void Viewport::scrollForward(int elements) {
-	moveTowardBound<std::max<int>>(_selectedElement, -moveTowardBound<std::min<int>>(_firstElement, elements, std::max(_totalElements() - _viewableElements(), 0)), 0);
+	clamp<std::max<int>>(_selectedElement, -clamp<std::min<int>>(_firstElement, elements, std::max(_totalElements() - _viewableElements(), 0)), 0);
 }
 
 void Viewport::selectElement(int element) {
-	int elements = element - (_firstElement + _selectedElement);
-	if (elements < 0)
+	if (int elements = element - (_firstElement + _selectedElement); elements < 0)
 		moveBackward(-elements);
 	else
 		moveForward(elements);
