8e75497c7e76bca830b84fac14343b7d4211abb2
Justin T <jtullek@gmail.com>
2023-04-18 14:26:45 +0200

    Add macro names for Viewport member variables
    This should make the code easier to read.

--- src/ren/viewport.cpp
+++ src/ren/viewport.cpp
@@ -27,31 +27,43 @@ int clamp(int &value, int increment, int limit) {
 	return result;
 }
 
+#define first\
+	_firstElement
+
+#define selected\
+	_selectedElement
+
+#define viewable\
+	_viewableElements()
+
+#define total\
+	_totalElements()
+
 void Viewport::refresh() {
-	if (int scrolled = clamp<std::min<int>>(_firstElement, 0, std::max(_totalElements() - viewableElements(), 0)))
-		clamp<std::min<int>>(_selectedElement, -scrolled, std::min(_viewableElements(), _totalElements()) - 1);
-	else if (int moved = clamp<std::min<int>>(_selectedElement, 0, std::min(_viewableElements(), _totalElements()) - 1))
-		clamp<std::min<int>>(_firstElement, -moved, std::max(_totalElements() - viewableElements(), 0));
+	if (int scrolled = clamp<std::min<int>>(first, 0, std::max(total - viewable, 0)))
+		clamp<std::min<int>>(selected, -scrolled, std::min(viewable, total) - 1);
+	else if (int moved = clamp<std::min<int>>(selected, 0, std::min(viewable, total) - 1))
+		clamp<std::min<int>>(first, -moved, std::max(total - viewable, 0));
 }
 
 void Viewport::moveBackward(int elements) {
-	clamp<std::max<int>>(_firstElement, -elements - clamp<std::max<int>>(_selectedElement, -elements, 0), 0);
+	clamp<std::max<int>>(first, -elements - clamp<std::max<int>>(selected, -elements, 0), 0);
 }
 
 void Viewport::moveForward(int elements) {
-	clamp<std::min<int>>(_firstElement, elements - clamp<std::min<int>>(_selectedElement, elements, std::min(_viewableElements(), _totalElements()) - 1), std::max(_totalElements() - _viewableElements(), 0));
+	clamp<std::min<int>>(first, elements - clamp<std::min<int>>(selected, elements, std::min(viewable, total) - 1), std::max(total - viewable, 0));
 }
 
 void Viewport::scrollBackward(int elements) {
-	clamp<std::min<int>>(_selectedElement, -clamp<std::max<int>>(_firstElement, -elements, 0), std::min(_viewableElements(), _totalElements()) - 1);
+	clamp<std::min<int>>(selected, -clamp<std::max<int>>(first, -elements, 0), std::min(viewable, total) - 1);
 }
 
 void Viewport::scrollForward(int elements) {
-	clamp<std::max<int>>(_selectedElement, -clamp<std::min<int>>(_firstElement, elements, std::max(_totalElements() - _viewableElements(), 0)), 0);
+	clamp<std::max<int>>(selected, -clamp<std::min<int>>(first, elements, std::max(total - viewable, 0)), 0);
 }
 
 void Viewport::selectElement(int element) {
-	if (int elements = element - (_firstElement + _selectedElement); elements < 0)
+	if (int elements = element - (first + selected); elements < 0)
 		moveBackward(-elements);
 	else
 		moveForward(elements);
